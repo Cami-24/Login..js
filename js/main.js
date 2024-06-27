a499ab1c2c523f04d04a7d08c98e0c1e7f4c2e17
Cami-24 <cdgolondrina@gmail.com>
2024-06-27 13:32:54 -0600

    comentarios then

--- js/main.js
+++ js/main.js
@@ -12,5 +12,17 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
         body: JSON.stringify({ username, password }) //para intercambio de datos
     })
     .then(response => response.text())
+    /**then es un método que se ejecuta 
+     * una vez que la solicitud fetch se ha completado.
+     * response => response.text() es una función que toma la respuesta 
+     * de la solicitud fetch y la convierte en texto. */
+
     .then(data => console.log(data));
+
+    /**
+     * El segundo then se ejecuta después de que 
+     * la respuesta se haya convertido en texto.
+     * data => console.log(data) es una función que 
+     * toma los datos de la respuesta y los imprime en la consola del navegador
+     */
 });
