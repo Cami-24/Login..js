5fa7011f577faed7a7af1699c08f8ee709c46ce4
Cami-24 <cdgolondrina@gmail.com>
2024-06-27 14:23:19 -0600

    condicionales

--- php/conexion_bd.php
+++ php/conexion_bd.php
@@ -2,5 +2,13 @@
 
 [
     $conexion = mysqui_connect('localhost', 'root', '', 'login_squl');
+
+
+if($conexion){
+    echo 'Conectado existosamente a la base de datos';
+}else{
+    echo 'No se puede conectar a la base de datos';
+}
+
 ]
 ?>
