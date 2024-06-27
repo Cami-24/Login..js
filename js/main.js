b2b3433465a171e409ad7604d0144ff58a770fe5
Cami-24 <cdgolondrina@gmail.com>
2024-06-27 13:27:51 -0600

    mejoras js
    se hizo mas organizado, agregue method post para control de datos

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-let username = document.getElementById('username');
-let password = document.getElementById('password');
-let button = document.getElementById('button'); 
 
-button.addEventListener('click', (e) => { 
+document.getElementById('loginForm').addEventListener('submit', function (e) {
     e.preventDefault();
-    const data = {
-        username: username.value,
-        password: password.value
-    };
-    console.log(data);
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.text())
+    .then(data => console.log(data));
 });
