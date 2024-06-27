55f57d11fa6efe70d73646445db25cd91b12cb4a
Cami-24 <cdgolondrina@gmail.com>
2024-06-27 16:37:16 -0600

    php arreglado

--- php/conexion_bd.php
+++ php/conexion_bd.php
@@ -4,11 +4,11 @@
     $conexion = mysqli_connect('localhost', 'root', '', 'login_sql');
 
 
-if($conexion){
-    echo 'Conectado existosamente a la base de datos';
-}else{
-    echo 'No se puede conectar a la base de datos';
-}
+// if($conexion){
+//     echo 'Conectado existosamente a la base de datos';
+// }else{
+//     echo 'No se puede conectar a la base de datos';
+// }
 
 
 ?>
