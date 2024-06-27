1f7309d964d63d234dbac090adc2a6edd03b00e2
Cami-24 <cdgolondrina@gmail.com>
2024-06-27 16:54:57 -0600

    Update registro_usuario.php

--- php/registro_usuario.php
+++ php/registro_usuario.php
@@ -1,8 +1,11 @@
 <?php 
+    include'conexion_bd.php';
+    
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $datos = 'Agregue en usuarios (username, password)
-                values('$username','password')';
+    $datos = 'Agregue en usuarios(username, password) 
+            values('$username','$password')';
 
+    $ejecutar = mysqli_query($conexion, $datos);
 ?>
