0803b5b2b219120dc42f5f993df98c5a596ace59
Cami-24 <cdgolondrina@gmail.com>
2024-06-28 12:43:20 -0600

    submit y error null check

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
 
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const username = document.getElementById('loginUsername').value;
-    const password = document.getElementById('loginPassword').value;
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM completamente cargado y analizado');
 
-    fetch('/login', { //La función fetch se usa para hacer solicitudes HTTP. En este caso, está realizando una solicitud POST a la URL /login.
-        method: 'POST', //POST -> envia datos al servidor,  para crear o actualizar recursos.
-        headers: { //cabeceras, informa que hay datos con JSON
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password }) //para intercambio de datos
-    })
-    .then(response => response.text())
-    /**then es un método que se ejecuta 
-     * una vez que la solicitud fetch se ha completado.
-     * response => response.text() es una función que toma la respuesta 
-     * de la solicitud fetch y la convierte en texto. */
+    const form = document.getElementById('Form');
+    const username = document.getElementById('loginUsername');
+    const password = document.getElementById('loginPassword');
 
-    .then(data => console.log(data));
+    console.log('Formulario:', form);
+    console.log('Username:', username);
+    console.log('Password:', password);
 
-    /**
-     * El segundo then se ejecuta después de que 
-     * la respuesta se haya convertido en texto.
-     * data => console.log(data) es una función que 
-     * toma los datos de la respuesta y los imprime en la consola del navegador
-     */
+    if (form) {
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            if (username.value.length < 10) {
+                alert('Usuario muy corto');
+            } else {
+                console.log('Formulario enviado');
+            }
+        });
+    } else {
+        console.error('Formulario no encontrado');
+    }
 });
